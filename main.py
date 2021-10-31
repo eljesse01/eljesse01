7de2d0034f017138a3b1f5ee67110908e90b9857
Jesse Ofori <kwabs.amoah.ka@gmail.com>
2021-10-31 17:14:53 +0000

    python installation

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
+    return "Hello, World!"
+    
 if __name__ == "__main__":
-    app.run(debug=True)from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-if __TESTPAGE__ == "__main__":
     app.run(debug=True)
